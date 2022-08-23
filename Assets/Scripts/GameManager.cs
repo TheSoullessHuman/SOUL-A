90ea48eb409adde0c359ed6b865157d98e5373af
reja16 <89877584+reja16@users.noreply.github.com>
2022-08-22 23:08:12 -0500

    Pausa agregada

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Pausa;
+    public bool Pause;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pause = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Pausar();
+    }
+
+    void Pausar()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && Pause == false)
+        {
+            Pausa.SetActive(true);
+            Time.timeScale = 0f;
+            Pause = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && Pause == true)
+        {
+            Pausa.SetActive(false);
+            Time.timeScale = 1f;
+            Pause = false;
+        }
     }
 }
